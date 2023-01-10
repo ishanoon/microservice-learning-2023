ecbd0fd8a5b8e415345bd74dbdc96099522cb894
ishanoon <shanoongaspi@gmail.com>
2023-01-10 16:28:52 +0000

    task: resolve all route security issue and also being able to get eureka service authenticated via basic auth

--- shoppingmicroservice/api-gateway-service/src/main/java/com/example/apigatewayservice/config/SecurityConfig.java
+++ shoppingmicroservice/api-gateway-service/src/main/java/com/example/apigatewayservice/config/SecurityConfig.java
@@ -3,11 +3,11 @@ package com.example.apigatewayservice.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
-@EnableWebFlux
+@EnableWebFluxSecurity
 @Configuration
 public class SecurityConfig {
 
@@ -17,10 +17,9 @@ public class SecurityConfig {
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(authorisation ->
                         authorisation
-                                .pathMatchers("/eureka/**","/api/v1/order/addOrder","/api/v1/inventory/*")
-                                .permitAll()
-                                .anyExchange()
-                                .authenticated())
+                                .pathMatchers("/eureka/**").permitAll()
+                                .anyExchange().authenticated()
+                )
                 .oauth2ResourceServer(ServerHttpSecurity.OAuth2ResourceServerSpec::jwt);
 
         return httpSecurity.build();
---
+++ shoppingmicroservice/discovery-server/src/main/java/io/shanoon/discoveryserver/config/SecurityConfig.java
@@ -0,0 +1,59 @@
+package io.shanoon.discoveryserver.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Value("${eureka.username}")
+    private String username;
+
+    @Value("${eureka.password}")
+    private String password;
+
+
+    @Bean
+    public SecurityFilterChain webFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.
+                csrf(AbstractHttpConfigurer::disable)
+                .httpBasic()
+                .and()
+                .authorizeHttpRequests(request ->
+                        request.anyRequest().authenticated());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(PasswordEncoder passwordEncoder) {
+        UserDetails user = User.withUsername(username)
+                .password(passwordEncoder.encode(password))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder.encode("admin"))
+                .roles("USER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return encoder;
+    }
+
+}
